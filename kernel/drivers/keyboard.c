eac359df1eca54051a5ff9c9217cf6c927cf8b4b
spr <spyridoula101@gmail.com>
2024-10-12 19:31:44 +0300

    Correct scancodes printed.2

--- kernel/drivers/keyboard.c
+++ kernel/drivers/keyboard.c
@@ -4,13 +4,20 @@
 #include "../src/vga.h"
 #include "keyboard.h"
 
-// Simple function to print scan code in decimal format
+// Function to convert scan code to int
+int ScanCodeToInt(uint8_t scanCode) {
+    return (int)scanCode; // Cast the uint8_t scanCode to int and return
+}
+
+// Function to print the scan code as an integer
 void printScanCode(uint8_t scanCode) {
     prints("Scan code: ");
-    char decString[4];  // Buffer for the decimal representation (3 digits + null)
-    // Convert scan code to decimal manually
-    int num = scanCode;
+    int num = ScanCodeToInt(scanCode); // Get the integer value of the scan code
+
+    // Convert the integer to a decimal string for printing
+    char decString[4];  // Buffer for the decimal representation (3 digits + null terminator)
     int i = 0;
+
     // Handle 0 explicitly
     if (num == 0) {
         decString[i++] = '0';
@@ -21,22 +28,27 @@ void printScanCode(uint8_t scanCode) {
             num /= 10; // Reduce number
         }
     }
+
     // Null-terminate the string
     decString[i] = '\0';
+
     // Reverse the string to get the correct order
     for (int j = 0; j < i / 2; j++) {
         char temp = decString[j];
         decString[j] = decString[i - j - 1];
         decString[i - j - 1] = temp;
     }
+
     // Print the decimal string
     prints(decString);
 }
 
+// Initialize the keyboard
 void initKeyboard() {
     irq_install_handler(1, &keyboardHandler);
 }
 
+// Keyboard interrupt handler
 void keyboardHandler(struct InterruptRegisters *regs) {
     uint8_t scanCode = inPortB(0x60) & 0x7F; // Get scan code
     uint8_t press = inPortB(0x60) & 0x80;    // Get press/release status
@@ -45,8 +57,8 @@ void keyboardHandler(struct InterruptRegisters *regs) {
 
     // Print press/release status
     if (press == 0) {
-        prints(":Pressed\n");
+        prints(": Pressed\n");
     } else {
-        prints(":Released\n");
+        prints(": Released\n");
     }
 }
