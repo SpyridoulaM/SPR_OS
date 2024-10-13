6d0527fbd3feee7a171270a4c516cb828c030bfe
spr <spyridoula101@gmail.com>
2024-10-13 17:27:23 +0300

    unneeded testing code deleted

--- kernel/drivers/keyboard.c
+++ kernel/drivers/keyboard.c
@@ -116,32 +116,3 @@ void keyboardHandler(struct InterruptRegisters *regs) {
         }
     }
 }
-
-// Simple function to print scan code in decimal format
-void printScanCode(uint8_t scanCode) {
-    prints("Scan code: ");
-    char decString[4];  // Buffer for the decimal representation (3 digits + null)
-    // Convert scan code to decimal manually
-    int num = scanCode;
-    int i = 0;
-    // Handle 0 explicitly
-    if (num == 0) {
-        decString[i++] = '0';
-    } else {
-        // Convert the number to decimal string
-        while (num > 0) {
-            decString[i++] = (num % 10) + '0'; // Get last digit and convert to char
-            num /= 10; // Reduce number
-        }
-    }
-    // Null-terminate the string
-    decString[i] = '\0';
-    // Reverse the string to get the correct order
-    for (int j = 0; j < i / 2; j++) {
-        char temp = decString[j];
-        decString[j] = decString[i - j - 1];
-        decString[i - j - 1] = temp;
-    }
-    // Print the decimal string
-    prints(decString);
-}
